1b92bdfe2725e1e45340f17ddf1f68bb4e8e346c
KevinAgesilas <k.agesilas@laposte.net>
2016-09-27 14:35:59 +0200

    Add main class

---
+++ src/tdd0/Main.java
@@ -0,0 +1,11 @@
+package tdd0;
+
+/**
+ * Created by keagesil on 27/09/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+    }
+}
+
